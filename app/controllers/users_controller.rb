b19f44fa51cef7b93610213824ae0e53a20f2567
kanade1177 <abkeybg111177@gmail.com>
2021-08-11 13:17:55 +0000

    InitialCommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+  
+  
   def show
     @user = User.find(params[:id])
     @books = @user.book_ids
   end
-
-
 
   def edit
     @user = User.find(params[:id])
