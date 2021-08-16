66576848edf4fe29c7703935097bcf8283638f33
kanade1177 <abkeybg111177@gmail.com>
2021-08-16 12:52:36 +0000

    InitialCommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,17 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @user = User.new
+    @user = current_user
+    @book = Book.new
   end
-  
-  
+
+
   def show
     @user = User.find(params[:id])
-    
+    @books = @user.books
+    @book = Book.new
   end
+
 
   def edit
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books
 
-   resources :users, only: [:new, :index, :create, :show, :edit, :update,]
+   resources :users
 end
 
 
