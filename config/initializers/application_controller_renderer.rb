c371c09f6fbb5c75314652a93d981278f52a1b08
kanade1177 <abkeybg111177@gmail.com>
2021-08-18 02:54:07 +0000

    InitialCommit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '353e699981cd5a722c6da373be864ddffefea525e6f115e818f2b3b2a35b53f6b4281a58e8284aefbad7646781c47874cd85a1f72852a700161508a864446a31'
