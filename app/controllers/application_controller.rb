250e7135dbde9ea1f708385b3864925dc7077507
kanade1177 <abkeybg111177@gmail.com>
2021-08-20 09:04:27 +0000

    REspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
 before_action :configure_permitted_parameters, if: :devise_controller?
 before_action :authenticate_user!, except: [:top, :about]
-before_action :correct_users,only: [:show]
+
 
 def after_sign_in_path_for(resource)
   user_path(current_user.id)
@@ -17,11 +17,6 @@ end
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 
-  def correct_users
-        @user = User.find(params[:id])
-      unless @user.id == current_user.id
-       redirect_to user_path(current_user.id)
-      end
-  end
+
 
 end
