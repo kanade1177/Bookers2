f6c61b00323bf78af6b198072e8a293b8c9eb99e
kanade1177 <abkeybg111177@gmail.com>
2021-09-10 12:58:59 +0000

    apply 7c controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+
+def index
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+private
+
 end
